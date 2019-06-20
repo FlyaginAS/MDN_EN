c6a85f5cce5d2d2b3110a209f9c1f41ab253dbee
ruscat <rus.reg.36@gmail.com>
2019-06-20 13:17:41 +0300

    jQuery 20%- css classes

--- 9_Introducing_JavaScript_objects/scripts/main.js
+++ 9_Introducing_JavaScript_objects/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-$('#content').css('color', 'red');
-$('section#content').css('color', 'blue');
-$('.intro').css('color','yellow');
-$('h3').css('background-color', 'grey');
-console.log($('#content').children());
+$('#my').css({
+    'color': 'red',
+    'background-color': 'yellow'
+});
+
